831589503398fbaaf2181a01a6aafb7f52e1b40a
Romain Pelisse <belaran@redhat.com>
2025-02-20 16:00:41 +0100

    rpe: jpa done

--- helloworld-rs/src/main/java/org/jboss/as/quickstarts/helloworld/rest/HelloWorld.java
+++ helloworld-rs/src/main/java/org/jboss/as/quickstarts/helloworld/rest/HelloWorld.java
@@ -4,10 +4,15 @@
  */
 package org.jboss.as.quickstarts.helloworld.rest;
 
-import jakarta.ws.rs.GET;
+//import jakarta.ws.rs.GET;
+//import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.inject.Inject;
+
 /**
  * A simple REST service which is able to say "Hello World!"
  *
@@ -16,12 +21,21 @@ import jakarta.ws.rs.core.MediaType;
  */
 
 @Path("/")
+@Consumes("application/json")
 public class HelloWorld {
 
-    @GET
-    @Path("/HelloWorld")
+    @Inject
+    private EdvigeService edvige;
+
+    @POST
+    @Path("/entry")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello World!";
+    @Consumes("application/json")
+    public String createEntry(Entry entry) {
+      if ( edvige != null ) {
+          edvige.createEntry(entry);
+          return "Entry created:" + entry;
+      }
+      return "Hello World!";
     }
 }
